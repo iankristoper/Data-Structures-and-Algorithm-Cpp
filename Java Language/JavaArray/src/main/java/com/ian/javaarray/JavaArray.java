767fa1fdfbf501e928a38695bc80a3f0cac14ae5
iankristoper <iankristoper26@gmail.com>
2025-02-09 01:48:43 +0800

    initial commit

---
+++ Java Language/JavaArray/src/main/java/com/ian/javaarray/JavaArray.java
@@ -0,0 +1,29 @@
+package com.ian.javaarray;
+
+
+
+import java.util.Scanner;
+
+
+
+public class JavaArray {
+
+    public static void main(String[] args) {
+              
+    Scanner scanner = new Scanner(System.in);      
+        
+    //example of array traversing - O(1) 
+    
+    
+    
+    
+        
+        
+    //example of array traversing - O(n)
+        
+        
+
+        
+      
+    }
+}
