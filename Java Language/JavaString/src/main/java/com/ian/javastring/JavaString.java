4737901ee552dd7fed39be593a84688891c680f2
iankristoper <iankristoper26@gmail.com>
2025-02-10 12:26:31 +0800

    initial changes

---
+++ Java Language/JavaString/src/main/java/com/ian/javastring/JavaString.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ian.javastring;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+public class JavaString {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
